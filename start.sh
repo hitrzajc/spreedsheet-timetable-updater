d29ae8bf87842157d6e3e00f24088ffed947b5ce
hitrzajc <tomazic.tadej@gmail.com>
2023-03-10 00:54:03 +0100

    mistake in path

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /opt/wash-timetable-updater/venv/activate
-python3 /opt/wash-timetable-updater/wash-timetable-updater.python3
+source /opt/wash-timetable-updater/venv/bin/activate
+python3 /opt/wash-timetable-updater/wash-timetable-updater.py
