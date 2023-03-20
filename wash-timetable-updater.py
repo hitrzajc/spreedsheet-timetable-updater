4d57db1540a15ca60d5f57f532f46a83db1dd831
hitrzajc <tomazic.tadej@gmail.com>
2023-03-20 15:14:41 +0100

    modified to work only for public sheets, and removed annoying auth2.0

--- wash-timetable-updater.py
+++ wash-timetable-updater.py
@@ -19,7 +19,7 @@ SPREADSHEET_ID = "1f4aUWR46jZeRy_sla25cyCBAnGf0xZ68gOiUmJ3Ekuo"
 # ID for testing
 #SPREADSHEET_ID = '14rNlHqmBvhNh8Codp8P059KVk8Ih3CypsmiEdGQBsNg'
 RANGE_PREFIX = 'Aktualno!'
-
+KEY = ""
 # [[RANGE_FROM, RANGE_TO]]
 RANGES = [["C25:I33", "C13:I21"],
           ["L25:R33", "L13:R21"]
@@ -42,24 +42,15 @@ def update_values(service, spreadsheet_id, range_name, value_input_option,
         return error
 
 def main():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    if os.path.exists('key.txt'):
+        with open("key.txt") as txt:
+            KEY = txt.readline() 
+    else:
+        print("key.txt is missing")
+        return
 
     try: 
-        service = build('sheets', 'v4', credentials=creds)
+        service = build('sheets', 'v4', developerKey=KEY)
         sheet = service.spreadsheets()
 
         empty = [["" for i in range(7)] for i in range(9)]
