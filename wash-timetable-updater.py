e835ac053af1c75d19a7167865f792e8ffad506b
hitrzajc <tomazic.tadej@gmail.com>
2023-04-19 22:52:12 +0200

    migrated to service account key, end of api key support

--- wash-timetable-updater.py
+++ wash-timetable-updater.py
@@ -1,22 +1,24 @@
 from __future__ import print_function
 
-
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from oauth2client.service_account import ServiceAccountCredentials
 
 import os.path
 from datetime import datetime
 from datetime import timedelta
 
-KEY = ""
 LOCATION = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__))) + "/"
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+CREDENTALS = service_account.Credentials.from_service_account_file(
+     LOCATION+'credentials.json', scopes=SCOPES)
 # ID for production 
 SPREADSHEET_ID = "1f4aUWR46jZeRy_sla25cyCBAnGf0xZ68gOiUmJ3Ekuo"
 # ID for testing
-#SPREADSHEET_ID = '14rNlHqmBvhNh8Codp8P059KVk8Ih3CypsmiEdGQBsNg'
-RANGE_PREFIX = 'Aktualno!'
+# SPREADSHEET_ID = '14rNlHqmBvhNh8Codp8P059KVk8Ih3CypsmiEdGQBsNg'
+#RANGE_PREFIX = 'Aktualno!'
 # [[RANGE_FROM, RANGE_TO]]
 RANGES = [["C25:I33", "C13:I21"],
           ["L25:R33", "L13:R21"]
@@ -37,16 +39,9 @@ def update_values(service, spreadsheet_id, range_name, value_input_option,
         print(f"An error occurred: {error}")
         return error
 
-def main():
-    if os.path.exists(LOCATION + 'key.txt'):
-        with open(LOCATION + "key.txt") as txt:
-            KEY = txt.readline() 
-    else:
-        print("key.txt is missing")
-        return
-
+def main():       
     try: 
-        service = build('sheets', 'v4', developerKey=KEY)
+        service = build('sheets', 'v4', credentials=CREDENTALS)
         sheet = service.spreadsheets()
 
         empty = [["" for i in range(7)] for i in range(9)]
